e5de5bfe76ff25cbf3f2de34f0320696c3f56496
Nick Beaujean <niekvenlo@gmail.com>
2017-05-28 16:14:09 -0700

    Added hacky GUI, still missing no. of shots and endgame

--- three.rb
+++ three.rb
@@ -1,47 +1,77 @@
 # BATTLESHIP GAME
+# TODO
+# 1. Refactor deploy_ship # Done
+# 2. Build visualiser
+# 3. Fix Game.new mess    # Changed. Not sure it's better
+# 4. Rename hit? method   # Let's not
 
-class Game
-  def self.play
-    @players = [1,2].map { |i| Player.new(i) }
-    @players.each { |player| player.deploy_ships }
-    while @players.each { |player| player.fire }
+class String
+  def color(a)
+    case a
+    when :green then "\033[32m#{self}\033[0m"
+    when :blue  then "\033[36m#{self}\033[0m"
+    when :red   then "\033[31m#{self}\033[0m"
+    when :white then "\033[37m#{self}\033[0m"
+    when :brown then "\033[33m#{self}\033[0m"
     end
-  end
-
-  def self.ship_types
-    @ship_types = { carrier: 5, battleship: 4, cruiser: 3, submarine: 3, destroyer: 2 }
-  end
-
-  def self.opponent(player_id)
-    @players[2-player_id] # Hacky
   end
 end
 
 class Player
+  attr_writer :opponent
+  attr_reader :my_shots_fired, :player_color
+
   # Public: Initialize a Player.
   #
   # player_id - An Integer identifying the Player.
   def initialize(player_id)
     @player_id = player_id
-    # DEV : Prefilled hash for development purposes. Reset to empty hash.
-    #@my_ship_locations = {:A1=>:carrier, :A2=>:carrier, :A3=>:carrier, :A4=>:carrier, :A5=>:carrier, :B1=>:battleship, :B2=>:battleship, :B3=>:battleship, :B4=>:battleship, :C1=>:cruiser, :C2=>:cruiser, :C3=>:cruiser, :D1=>:submarine, :D2=>:submarine, :D3=>:submarine, :E1=>:destroyer, :E2=>:destroyer}   # Hash[coordinate] = ship_type
+    player_colors = [:green, :blue]
+    @player_color = player_colors[player_id-1]
     @my_ship_locations = {}
     @my_shots_fired = {}      # Hash[coordinate] = hit/miss
+    @opponent
+    @ship_types = { carrier: 5 }#, battleship: 4, cruiser: 3, submarine: 3, destroyer: 2 }
+  end
+
+  def paint(whose)
+    1.upto(10) do |number|
+      line = ""
+      "A".upto("J") do |letter|
+        line << paint_helper(whose, (letter+number.to_s).to_sym)#.center(4)
+      end
+      puts line
+    end
+  end
+
+  def paint_helper(whose, position)
+    if whose == :mine
+      return "▓▓".color :red if @opponent.my_shots_fired.key?(position) && @my_ship_locations.key?(position)
+      return "▓▓".color :brown if @opponent.my_shots_fired.key?(position)
+      return "▓▓".color :white if @my_ship_locations.key?(position)
+      "▓▓".color @player_color
+    else
+      return "▓▓".color :red if !@opponent.hit?(position).empty? && @my_shots_fired.key?(position)
+      return "▓▓".color :brown if @my_shots_fired.key?(position)
+      "▓▓".color @opponent.player_color
+    end
   end
 
   # Public: Deploys a Player's ships
   #
   # Returns Boolean
   def deploy_ships
-    puts "\nPlayer #{@player_id}:"
-    Game.ship_types.each do |ship|
+    puts "\nPlayer #{@player_id}:".color @player_color
+    paint(:mine)
+    @ship_types.each do |ship|
       puts "Deploy your #{ship[0]} (length #{ship[1]})"
       positions = gets.chomp
       redo unless deploy_ship(positions, ship)
       end
-    puts "All ships succesfully deployed"
-    #sleep(0.5)
-    #puts "\n"*40
+    puts "All ships succesfully deployed".freeze
+    puts "Press enter".freeze
+    gets
+    puts "\n"*40
     return true # Why am I doing this?
   end
 
@@ -49,16 +79,30 @@ class Player
   #
   # Returns a Boolean
   def fire
-    opponent = Game.opponent(@player_id)
-    puts "Player #{@player_id} targeting"
+    paint(:mine)
+    paint(:opponent)
+    puts "Player #{@player_id} targeting"#.color @player_color
     input = gets.chomp
     target = input.scan(/\b(\w\d)\b/).flatten.map(&:to_sym)
-    hits = opponent.hit?(*target)
-    hits.each { |hit| puts "Hit a #{hit[1].capitalize} at #{hit[0]}!" }
+    shots = {}
+    target.each do |shot|
+      shots[shot] = :splash
+    end
+    hits = @opponent.hit?(*target)
+    hits.each do |hit|
+      puts "Hit a #{hit[1].capitalize} at #{hit[0]}!"
+      shots[hit.first] = hit.last
+    end
+    @my_shots_fired.merge!(shots)
+    paint(:mine)
+    paint(:opponent)
+    puts "Press enter"
+    gets
+    puts "\n"*40
     true
   end
 
-  # Internal: Check whether positions in the Player's space are occupied.
+  # Public: Check whether positions in the Player's space are occupied.
   #
   # positions - Receives 1+ Symbols representing game spaces.
   #
@@ -86,44 +130,52 @@ class Player
   #   # => true
   #
   # Returns Boolean
-  def deploy_ship(coordinates, ship)
-    letters = coordinates.scan(/\b(\w)\d\b/).uniq.flatten
-    numbers = coordinates.scan(/\b\w(\d)\b/).uniq.flatten.map(&:to_i)
-    return false if (letters + numbers).length != 3
+  def deploy_ship(positions, ship)
     shipname, shiplength = ship
-    provisional_ship_locations = {}
-    if letters.length == 1 # Vertical placement
-      letter = letters[0]
+    letters = positions.scan(/\b(\w)\d\b/).uniq.flatten
+    numbers = positions.scan(/\b\w(\d)\b/).uniq.flatten.map(&:to_i)
+
+    return false if (letters + numbers).size != 3
+    if letters.size == 1
+      orientation = :vertical
+      letter = letters.first
       bow, stern = numbers
-      if stern - bow == shiplength - 1
-        (bow..stern).each do |number|
-          coordinate = (letter+number.to_s).to_sym
-          provisional_ship_locations[coordinate] = shipname
-          return false unless hit?(coordinate).empty?
-        end
-        @my_ship_locations.merge!(provisional_ship_locations)
-        puts "#{shipname.capitalize} deployed"
-        return true
-      end
-    elsif numbers.length == 1 # Horizontal placement
-      number = numbers[0]
+    elsif numbers.size == 1
+      orientation = :horizontal
+      number = numbers.first
       bow, stern = letters
-      if stern.ord - bow.ord == shiplength - 1
-        (bow..stern).each do |letter|
-          coordinate = (letter+number.to_s).to_sym
-          provisional_ship_locations[coordinate] = shipname
-          return false unless hit?(coordinate).empty?
-        end
-        @my_ship_locations.merge!(provisional_ship_locations)
-        puts "#{shipname.capitalize} deployed"
-        return true
-      end
     else
-      puts "Nope, try again"
       return false
     end
-  end
 
+    provisional_ship_locations = {}
+    (bow..stern).each do |var|
+      number = var if orientation == :vertical
+      letter = var if orientation == :horizontal
+      coordinate = (letter+number.to_s).to_sym
+      provisional_ship_locations[coordinate] = shipname
+      return false unless hit?(coordinate).empty?
+    end
+    @my_ship_locations.merge!(provisional_ship_locations)
+    puts "#{shipname.capitalize} deployed"
+    paint(:mine)
+    return true
+  end
 end
 
-Game.play
+puts "Welcome to Battleship".freeze
+puts "This is a game for two players".freeze
+
+p1 = Player.new(1)
+p2 = Player.new(2)
+
+p1.opponent = p2
+p2.opponent = p1
+
+p1.deploy_ships
+p2.deploy_ships
+
+while true
+  p1.fire
+  p2.fire
+end
